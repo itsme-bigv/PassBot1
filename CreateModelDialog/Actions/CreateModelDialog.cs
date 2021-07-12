67bb0ddc9509d6e1c82ff3e0f064644589dee34d
itsme-bigv <66802040+itsme-bigv@users.noreply.github.com>
2021-07-12 13:22:49 +0200

    Projektdateien hinzufügen.

---
+++ CreateModelDialog/Actions/CreateModelDialog.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using AdaptiveExpressions.Properties;
+using Microsoft.Bot.Builder.Dialogs;
+using Newtonsoft.Json;
+using alps.net_api;
+
+
+namespace CreateModelDialog
+{
+    public class CreateModelDialog : Dialog
+    {
+        [JsonConstructor]
+        public CreateModelDialog([CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
+            : base()
+        {
+            // enable instances of this command as debug break point
+            RegisterSourceLocation(sourceFilePath, sourceLineNumber);
+        }
+
+        [JsonProperty("$kind")]
+        public const string Kind = "CreateModelDialog";
+
+        [JsonProperty("wishToCreate")]
+        public BoolExpression CreateModel { get; set; }
+
+        [JsonProperty("createdModel")]
+        public PassProcessModel Model { get; set; }
+
+        public override Task<DialogTurnResult> BeginDialogAsync(DialogContext dc, object options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+
+            if (CreateModel.GetValue(dc.State))
+            {
+                //Create Model and save it to state
+                this.Model = new PassProcessModel();
+
+                //Set default layer
+                Model.addLayer("defaultLayer");
+            }
+
+
+            return dc.EndDialogAsync(result: Model, cancellationToken: cancellationToken);
+        }
+    }
+}
---
+++ CreateModelDialog/CreateModelDialogBotComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs.Declarative;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CreateModelDialog
+{
+    public class CreateModelDialogBotComponent : BotComponent
+    {
+        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            // Anything that could be done in Startup.ConfigureServices can be done here.
+            // In this case, the CreateModelDialog needs to be added as a new DeclarativeType.
+            services.AddSingleton<DeclarativeType>(sp => new DeclarativeType<CreateModelDialog>(CreateModelDialog.Kind));
+        }
+    }
+}
